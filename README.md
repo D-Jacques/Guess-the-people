521d44adb5241113eee03bc08f6e10707c991be3
jacques <jacques.ducroux@gmail.com>
2024-07-07 16:35:46 +0200

    Added fixtures and documented readme

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 Installation du projet:
 
+prérequis : Avoir installé php 8.3, MySQL et phpmyadmin
+            Avoir installé Node versione 20.12.0, npm 10.8.1
+            Avoir installé composer v2.7.2, SymfonyCLI 5.9.1
+
 - Cloner le projet
+
+## Pour le backoffice
+
 - Dans le dossier back lancer : composer install
 - Lancer la commande suivante pour mettre en place la base de données (Il faut bien avoir créer la base guess-the-people avant)
 
@@ -20,3 +27,26 @@ symfony console doctrine:fixtures:load
 ```sh
 symfony console tailwind:build
 ```
+- Lancer le serveur
+
+```sh
+symfony console tailwind:build
+```
+
+Le serveur devrait se lancer sur le localhost port 8000
+
+## Pour le front office :
+
+- Dans le dossier front lancer : 
+
+```sh
+npm install
+```
+
+- Lancer le serveur :
+
+```sh
+npm run dev
+```
+
+Le serveur devrait se lancer sur le serveur localhost port 5173.
--- back/src/DataFixtures/RiddleCardFixture.php
+++ back/src/DataFixtures/RiddleCardFixture.php
@@ -26,10 +26,45 @@ class RiddleCardFixture extends Fixture
                 "name" => "Anne-sophie lapix",
                 "description" => "Célèbre présentatrice sur le journal de France 2 qui a aussi présenté C a vous sur France 5",
                 "picture" => ""
-            ]
+            ],
+            3 => [
+                "name" => "Christopher Nolan",
+                "description" => "Homme du cinéma, il a réalisé entre autres les films batman, the dark knight et the dark knight rises",
+                "picture" => ""
+            ],
+            4 => [
+                "name" => "Barack Obama",
+                "description" => "Ancient président, il avait fait campagne avec son célèbre slogan \"Yes We Can\" ",
+                "picture" => ""
+            ],
+            5 => [
+                "name" => "Thomas Pesquet",
+                "description" => "Spationaute Français, il a été envoyé dans l'espace dans le cadre de la mission proxima",
+                "picture" => ""
+            ],
+            6 => [
+                "name" => "Bill Gates",
+                "description" => "Grand informaticien, il est cofondateur de Microsoft",
+                "picture" => ""
+            ],
+            7 => [
+                "name" => "Hugo Décrypte",
+                "description" => "Jeune youtubeur, il réalise de courtes vidéos expliquant l'actualité à destination des jeunes",
+                "picture" => ""
+            ],
+            8 => [
+                "name" => "Lionnel Messi",
+                "description" => "Grand joueur Argentin, il est célèbre mondialement dans le milieu du football",
+                "picture" => ""
+            ],
+            9 => [
+                "name" => "Linus Torvald",
+                "description" => "Informaticien d'origine finlandaise, il est le créateur de Linux et de git",
+                "picture" => ""
+            ],
         ];
 
-        for ($i=0; $i < 3; $i++) { 
+        for ($i=0; $i < count($fixtureDatas); $i++) { 
             $riddle = (new RiddleCard())
             ->setName($fixtureDatas[$i]["name"])
             ->setDescription($fixtureDatas[$i]["description"]);
